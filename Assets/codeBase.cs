0e962b7b6facad672ea29a84a79c4062912de6b8
valent16 <v.gilbert630@laposte.net>
2018-11-11 02:37:43 +0100

    add reset button + link listeners on buttons

--- Assets/codeBase.cs
+++ Assets/codeBase.cs
@@ -15,8 +15,19 @@ public class codeBase : MonoBehaviour {
     public InputField phiInput;
 
     public GameObject qBitArrow;
-    
-    
+    public Toggle vectorPackToggle;
+
+
+    public Button PaulyXGate;
+    public Button PaulyYGate;
+    public Button PaulyZGate;
+    public Button HadamardGate;
+
+    public Button ResetStateButton;
+
+    //List used when a range of vectors is used
+    public List<GameObject> qubitArrows = new List<GameObject>();
+
     // Use this for initialization
     void Start () {
         thetaSlider.value = 0;
@@ -25,12 +36,40 @@ public class codeBase : MonoBehaviour {
         thetaSlider.onValueChanged.AddListener(delegate { thetaSliderChanged(); });
         phiSlider.onValueChanged.AddListener(delegate { phiSliderChanged(); });
         
+        //Sets listeners on gates buttons
+        PaulyXGate.onClick.AddListener(delegate { setPaulyXGateTransform(); });
+        PaulyYGate.onClick.AddListener(delegate { setPaulyYGateTransform(); });
+        PaulyZGate.onClick.AddListener(delegate { setPaulyZGateTransform(); });
+        HadamardGate.onClick.AddListener(delegate { setHadamardGateTransform(); });
+
     }
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+
+
+    void setPaulyXGateTransform()
+    {
+
+    }
+
+    void setPaulyYGateTransform()
+    {
+
+    }
+
+    void setPaulyZGateTransform()
+    {
+
+    }
+
+    void setHadamardGateTransform()
+    {
+
+    }
+
 
     void thetaSliderChanged()
     {
