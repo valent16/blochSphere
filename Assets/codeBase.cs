0b7e7b72f2b611bca57d28f440993f27cb327c33
valent16 <v.gilbert630@laposte.net>
2018-11-11 23:23:58 +0100

    axis X, Y, Z added, reset function operational, listener events created, X Pauli matrix to be confirmed

--- Assets/codeBase.cs
+++ Assets/codeBase.cs
@@ -5,8 +5,12 @@ using UnityEngine.UI;
 
 public class codeBase : MonoBehaviour {
 
-    public float thetaValue;
-    public float phiValue;
+    public const int ThetaInitValue = 0;
+    public const int PhiInitValue = 0;
+
+
+    public float thetaValue = ThetaInitValue;
+    public float phiValue = PhiInitValue;
 
     public Slider thetaSlider;
     public Slider phiSlider;
@@ -30,29 +34,54 @@ public class codeBase : MonoBehaviour {
 
     // Use this for initialization
     void Start () {
-        thetaSlider.value = 0;
-        phiSlider.value = 0;
-        
+        initializeValues();
+        initializeListeners();
+    }
+
+    //Initilizes the listeners of each canvas items
+    private void initializeListeners()
+    {
+        //Sets liteners on sliders
         thetaSlider.onValueChanged.AddListener(delegate { thetaSliderChanged(); });
         phiSlider.onValueChanged.AddListener(delegate { phiSliderChanged(); });
-        
+
         //Sets listeners on gates buttons
         PaulyXGate.onClick.AddListener(delegate { setPaulyXGateTransform(); });
         PaulyYGate.onClick.AddListener(delegate { setPaulyYGateTransform(); });
         PaulyZGate.onClick.AddListener(delegate { setPaulyZGateTransform(); });
         HadamardGate.onClick.AddListener(delegate { setHadamardGateTransform(); });
 
+        //Sets listener on the reset button
+        ResetStateButton.onClick.AddListener(delegate { resetQuBitState(); });
+
     }
-	
+
+    //Initializes field values 
+    private void initializeValues()
+    {
+        thetaSlider.value = ThetaInitValue;
+        phiSlider.value = PhiInitValue;
+
+        thetaInput.contentType = InputField.ContentType.IntegerNumber;
+        thetaInput.text = ThetaInitValue.ToString();
+        
+        phiInput.contentType = InputField.ContentType.IntegerNumber;
+        phiInput.text = PhiInitValue.ToString();
+    }
+
 	// Update is called once per frame
 	void Update () {
 		
 	}
-
-
+    
     void setPaulyXGateTransform()
     {
+        thetaSlider.value = 180 - thetaSlider.value;
+        //phiSlider.value = (180 + phiSlider.value)%360;
 
+        phiSlider.value = 360 - phiSlider.value;
+        thetaSliderChanged();
+        phiSliderChanged();
     }
 
     void setPaulyYGateTransform()
@@ -68,6 +97,11 @@ public class codeBase : MonoBehaviour {
     void setHadamardGateTransform()
     {
 
+    }
+
+    private void resetQuBitState()
+    {
+        initializeValues();
     }
 
 
@@ -91,6 +125,26 @@ public class codeBase : MonoBehaviour {
     void updateQBitVector()
     {
         qBitArrow.transform.rotation = Quaternion.Euler(this.thetaValue, this.phiValue, 0);
+        //qBitArrow.transform.rotation = Quaternion.LookRotation(new Vector3(getZValue(), getYValue(), getXValue()));
+        //Debug.Log("X:" + getXValue() + "--- Y:" + getYValue() + "--- Z:" + getZValue());
+    }
+
+    //return the X value of the direction vector
+    float getXValue()
+    {
+        return Mathf.Sin(phiValue * Mathf.PI / 180) * Mathf.Sin(thetaValue * Mathf.PI / 180 / 2);
+    }
+
+    //return the Y value of the direction vector
+    float getYValue()
+    {
+        return Mathf.Cos(phiValue * Mathf.PI / 180) * Mathf.Sin(thetaValue * Mathf.PI / 180 / 2);
+    }
+
+    //return the Z value of the direction vector
+    float getZValue()
+    {
+        return Mathf.Cos(thetaValue * Mathf.PI / 180 /2);
     }
 
 }
